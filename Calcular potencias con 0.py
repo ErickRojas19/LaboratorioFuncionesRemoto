18587fba7965d4b297fb722a708c1b1eee34cb02
ErickRojas19 <47455082+ErickRojas19@users.noreply.github.com>
2019-03-29 11:58:04 -0500

    Add files via upload

---
+++ Calcular potencias con 0.py
@@ -0,0 +1,16 @@
+def a_power_b():
+
+    
+    prod=1
+    for i in range(1,b+1):
+        prod=prod*a
+        print(prod)
+while True:
+        
+    a=int(input("Digite un numero:"))
+    b=int(input("Digite el numero a elevar:"))
+    if prod==0:
+        break
+
+
+a_power_b()
